c4eeb9b8d706d70abc9c0e1ea3cb4cb32cbdc471
akosimico <jhaynoelbarnedo@gmail.com>
2024-09-06 12:16:23 +0800

    Add files via upload

---
+++ Barnido/Barnido/NowServing.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Barnido
+{
+    partial class NowServing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 108);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // NowServing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 172);
+            this.Controls.Add(this.label1);
+            this.Name = "NowServing";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "NowServing";
+            this.Load += new System.EventHandler(this.NowServing_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Barnido/Barnido/NowServing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barnido
+{
+    public partial class NowServing : Form
+    {
+        private Timer timer;
+        public NowServing()
+        {
+            InitializeComponent();
+        }
+
+        private void NowServing_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = (1 * 1000);
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Start();
+
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DisplayFirstQueue(CashierClass.CashierQueue);
+        }
+        private void DisplayFirstQueue(Queue<string> cashierQueue)
+        {
+            if (CashierClass.CashierQueue.Count > 0)
+            {
+                label1.Text = CashierClass.CashierQueue.Peek();
+            }
+        }
+    }
+}
